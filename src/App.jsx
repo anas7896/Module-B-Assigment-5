95506b6972c3b1e1a5b9864783898f63f6dc5a57
anas7896 <anasawan0092@gmail.com>
2024-07-03 16:32:38 +0500

    assigment-5 done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Img from './Img/Img'
+
+const App = () => {
+  return (
+    <div>
+      <Img/>
+    </div>
+  )
+}
+
+export default App
+
---
+++ src/Img/Img.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './Img.module.css'
+
+const Img = () => {
+  return (
+    <div>
+      <img className={styles.img}src="https://miro.medium.com/v2/resize:fit:1400/1*kxBdslclglg4zgCw0NMIIA.png" alt="" />
+
+      <h1>REACT + VITE</h1> 
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, accusamus?</p>
+    </div>
+  )
+}
+
+export default Img;
